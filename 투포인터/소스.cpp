6f149def2802487e16b52d592188baf5aba5c08b
jungwon <62919440+ap3334@users.noreply.github.com>
2021-11-18 11:01:55 +0900

    Add files via upload

---
+++ 투포인터/소스.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	
+	int N, M, temp;
+	vector<int> A, B;
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		A.push_back(temp);
+	}
+	for (int i = 0; i < M; i++) {
+		cin >> temp;
+		B.push_back(temp);
+	}
+
+	int a = 0, b = 0;
+
+	for (int i = 0; i < N + M; i++) {
+		if (a == N || b == M) break;
+		else if (A[a] > B[b]) {
+			cout << B[b] << " ";
+			b++;
+		}
+		else {
+			cout << A[a] << " ";
+			a++;
+		}
+	}
+
+	while (a != N) {
+		cout << A[a] << " ";
+		a++;
+	}
+	while (b != M) {
+		cout << B[b] << " ";
+		b++;
+	}
+
+}
