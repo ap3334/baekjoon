8d0697e72195f6779b7da3529375b310811a68e9
jungwon <62919440+ap3334@users.noreply.github.com>
2021-12-01 15:19:56 +0900

    Create 15721.cpp

---
+++ 완전탐색/15721.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	int A, T;
+	bool is_degi;
+	int bbun = 0, degi = 0, n = -1;
+
+	cin >> A >> T >> is_degi;
+
+	int cnt = 2;
+	while (1) {
+
+		for (int i = 0; i < 2; i++) {
+			bbun++; n++;
+			if (!is_degi && bbun == T) {
+				cout << n % A;
+				return 0;
+			}
+			degi++; n++;
+			if (is_degi && degi == T) {
+				cout << n % A;
+				return 0;
+			}
+		}
+
+		for (int i = 0; i < cnt; i++) {
+			bbun++; n++;
+			if (!is_degi && bbun == T) {
+				cout << n % A;
+				return 0;
+			}
+		}
+
+		for (int i = 0; i < cnt; i++) {
+			degi++; n++;
+			if (is_degi && degi == T) {
+				cout << n % A;
+				return 0;
+			}
+		}
+
+		cnt++;
+	}
+	
+}
